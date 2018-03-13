da8c86b6f8e31fcc01ad9b445d706e374f903716
Mark Price <devslopes@marks-mbp.lan>
2018-03-13 15:15:20 -0700

    listing wallet accounts

--- bin/wallet-cli.ts
+++ bin/wallet-cli.ts
@@ -10,10 +10,20 @@ const CFonts = require('cfonts');
 
 declare let process: any;
 const args = process.argv.slice(2);
+const homePath = `${os.homedir()}/cache-wallets`;
+let walletPaths: Array<string>;
+// let selectedWallet: SimpleWallet;
 
 CFonts.say('Cache', { colors: ['cyan']});
 if (args.length === 0) {
 	console.log(`Usage:
+
+	cache wallet list
+		Lists all of the wallets available in your cache-wallets directory
+	
+	cache wallet select <number>
+		Choose the wallet you want to work with ie 'cache wallet select 3'
+	
 	cache balance
 		Gets your current wallet balance and public address
 	
@@ -31,12 +41,12 @@ const downloadWallet = (wallet: SimpleWallet, address: string) => {
 	`));
 	const addAbb = address.substring(0,6);
 	const stamp = new Date().toISOString().substring(0,10);
-	const homePath = `${os.homedir()}/cache-wallets`;
+
 	if (!fs.existsSync(homePath)) {
 		fs.mkdirSync(homePath);
 	}
 	const path = `${homePath}/${addAbb}-${stamp}-cache.wlt`;
-	fs.writeFile(path, wallet.encryptedPrivateKey.encryptedKey, (_) => {
+	fs.writeFile(path, wallet.writeWLTFile(), (_) => {
 		console.log(green(`\nDownloaded wallet to ${path}\n`));
 	});
 };
@@ -82,14 +92,52 @@ This password will be used to encrypt your private key and make working with you
 	})
 };
 
-const main = () => {
+const listWallets = async () => {
+	console.log(white('Fetching wallets...\n'));
+
+	loadWalletPaths(() => {
+		if (walletPaths.length === 0) {
+			console.log(white(`No wallets found. Create a new wallet or place an existing .wlt file
+in ${homePath}\n`));
+			process.exit(1);
+		}
+		for (let x = 0; x < walletPaths.length; x++) {
+			console.log(`${x} - ${walletPaths[x]}`);
+		}
+		console.log('\n')
+	});
+};
+
+const loadWalletPaths = (onLoaded?: () => void) => {
+	// Load wallet paths behind the scenes automatically
+	fs.readdir(homePath, (_, files) => {
+		let paths: Array<string> = [];
+		for (let x = 0; x < files.length; x++) {
+			const str = files[x].substring(files[x].length - 3, files[x].length);
+			if (str === 'wlt') {
+				paths.push(files[x]);
+			}
+		}
+		walletPaths = paths;
+		if (onLoaded) {
+			onLoaded();
+		}
+	});
+};
+
+const main = async () => {
 	if (args[0] === 'wallet') {
 		if (args[1] === 'create') {
 			pwd();
+		} else if (args[1] === 'list') {
+			await listWallets();
+		} else if (args[1] === 'select') {
+
 		}
 	}
 };
 
+loadWalletPaths();
 main();
 
 process.on('uncaughtException', function(_) {
--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -1,9 +1,18 @@
-import { Account, NEMLibrary, NetworkTypes, Password, SimpleWallet } from 'nem-library';
+import {
+	Account, Address, EmptyMessage, MosaicHttp, MosaicId, NEMLibrary, NetworkTypes, Password, SimpleWallet, TimeWindow,
+	TransferTransaction
+} from 'nem-library';
+import { Observable } from 'rxjs/Observable';
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
 
 const WALLET_NAME = 'cache wallet';
 declare let process: any;
 
-NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+const namespace = 'devslopes';
+const cache = 'cache';
+const cacheId = new MosaicId(namespace, cache);
+const mosaicHttp = new MosaicHttp();
+
 const privateKey: string = process.env.PRIVATE_KEY;
 
 export const getWallet = (password: string): Account => {
@@ -15,4 +24,16 @@ export const getWallet = (password: string): Account => {
 export const createSimpleWallet = (password: string): SimpleWallet => {
 	const pass = new Password(password);
 	return SimpleWallet.create(WALLET_NAME, pass);
+};
+
+export const sendCache = () => {
+	Observable.from([cacheId])
+		.flatMap(mosaic => mosaicHttp.getMosaicTransferableWithAmount(mosaic, 10))
+		.toArray()
+		.map(mosaics => TransferTransaction.createWithMosaics(
+			TimeWindow.createWithDeadline(),
+			new Address('blah'),
+			mosaics,
+			EmptyMessage))
+
 };
